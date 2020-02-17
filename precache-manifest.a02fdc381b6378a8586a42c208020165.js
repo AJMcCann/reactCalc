8ef5d9423dedc103b8d68b29358f808b94196429
Aden McCann <aden.mccann@gmail.com>
2020-02-16 23:02:33 -0700

    Updates

---
+++ precache-manifest.a02fdc381b6378a8586a42c208020165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5de4eb094a7e4774619ce5a832554c",
+    "url": "/reactCalc/index.html"
+  },
+  {
+    "revision": "f9d1a57ecddbeb682a82",
+    "url": "/reactCalc/static/css/main.ab77d264.chunk.css"
+  },
+  {
+    "revision": "9cab757c247a4ed57889",
+    "url": "/reactCalc/static/js/2.4f190b27.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactCalc/static/js/2.4f190b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d1a57ecddbeb682a82",
+    "url": "/reactCalc/static/js/main.c88576ae.chunk.js"
+  },
+  {
+    "revision": "15146cdb4dd3aa0e29f0",
+    "url": "/reactCalc/static/js/runtime-main.34ff39ec.js"
+  }
+]);
